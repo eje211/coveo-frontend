1acba145fc122f0425a2b61865550c4bb4e8b1e4
Emmanuel Eytan <eje211@gmail.com>
2018-11-27 19:52:20 -0500

    Fixed production build base directory.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  outputDir: 'public'
+  outputDir: undefined,
+  baseUrl: '/demo',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
